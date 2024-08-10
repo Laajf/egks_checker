f55a12caa934792d6c9097b7ec8ec1bcc67659d4
Laajf <krym4rtem@yandex.ru>
2024-08-10 22:32:49 +0300

    функция

---
+++ Dockerfile
@@ -0,0 +1,44 @@
+# Используем официальный образ Python
+FROM python:3.12-slim
+
+RUN apt-get update && apt-get install -y \
+    curl \
+    wget \
+    unzip \
+    gnupg \
+    ca-certificates \
+    lsb-release \
+    libnss3 \
+    libgdk-pixbuf2.0-0 \
+    libatk-bridge2.0-0 \
+    libx11-xcb1 \
+    libxcomposite1 \
+    libxdamage1 \
+    libxrandr2 \
+    libgtk-3-0 \
+    libgbm-dev \
+    libasound2
+
+
+# Добавляем репозиторий Google Chrome
+RUN wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add - && \
+    sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
+
+# Устанавливаем Google Chrome
+RUN apt-get update && apt-get install -y google-chrome-stable
+
+# Устанавливаем ChromeDriver с помощью webdriver-manager
+RUN pip install webdriver-manager
+
+# Устанавливаем рабочую директорию
+WORKDIR /app/
+
+# Копируем проект и устанавливаем зависимости
+COPY pyproject.toml poetry.lock* /app/
+RUN pip install poetry && poetry install --no-root --only main
+
+# Копируем код приложения
+COPY . /app/
+
+# Указываем команду по умолчанию
+CMD ["poetry", "run", "python", "main.py"]
---
+++ main.py
@@ -0,0 +1,53 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+print("Starting the script...")
+
+chrome_options = Options()
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--headless=new")  # Вместо обычного "--headless"
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--disable-software-rasterizer")
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--remote-debugging-port=9222")  # для отладки
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=chrome_options)
+
+try:
+    driver.get("https://payberry.ru/pay/4253/")
+
+    # Вместо find_element_by_id используем find_element с By.ID
+    email_form = driver.find_element(By.CLASS_NAME, "field-row__input")
+    email_form.send_keys("000464198")
+
+    entered_value = email_form.get_attribute("value")
+    print(f"Entered value: {entered_value}")
+
+    if entered_value == "000464198":
+        print("наконец-то")
+        button = WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, ".g-btn.m-btn-lg.request-btn"))
+        )
+        button.click()
+        print("Button clicked")
+
+        print("Waiting for text 'Текущий билет:' to appear...")
+        ticket_element = WebDriverWait(driver, 15).until(
+            EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Текущий билет:')]"))
+        )
+        print("Text 'Текущий билет:' appeared.")
+
+        ticket_text = ticket_element.text
+        print(f"Found text: {ticket_text}")
+
+except Exception as e:
+    print(f"Element not found or another error occurred: {e}")
+finally:
+    driver.quit()
