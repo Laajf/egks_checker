7a7a8020542aa8fc6168a27a0c46fd7011d5f634
Laajf <krym4rtem@yandex.ru>
2024-08-10 23:17:06 +0300

    сделанна функция получения данных карты

--- main.py
+++ main.py
@@ -6,48 +6,52 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-print("Starting the script...")
 
-chrome_options = Options()
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--headless=new")  # Вместо обычного "--headless"
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--disable-software-rasterizer")
-chrome_options.add_argument("--disable-extensions")
-chrome_options.add_argument("--remote-debugging-port=9222")  # для отладки
+def get_data_money(number_card):
+    print("Starting the script...")
 
-service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=service, options=chrome_options)
+    chrome_options = Options()
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--headless=new")  # Вместо обычного "--headless"
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--disable-software-rasterizer")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--remote-debugging-port=9222")  # для отладки
 
-try:
-    driver.get("https://payberry.ru/pay/4253/")
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=chrome_options)
 
-    # Вместо find_element_by_id используем find_element с By.ID
-    email_form = driver.find_element(By.CLASS_NAME, "field-row__input")
-    email_form.send_keys("000464198")
+    try:
+        driver.get("https://payberry.ru/pay/4253/")
 
-    entered_value = email_form.get_attribute("value")
-    print(f"Entered value: {entered_value}")
+        # Вместо find_element_by_id используем find_element с By.ID
+        email_form = driver.find_element(By.CLASS_NAME, "field-row__input")
+        email_form.send_keys(f"{number_card}")
 
-    if entered_value == "000464198":
-        print("наконец-то")
-        button = WebDriverWait(driver, 10).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, ".g-btn.m-btn-lg.request-btn"))
-        )
-        button.click()
-        print("Button clicked")
+        entered_value = email_form.get_attribute("value")
+        print(f"Entered value: {entered_value}")
 
-        print("Waiting for text 'Текущий билет:' to appear...")
-        ticket_element = WebDriverWait(driver, 15).until(
-            EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Текущий билет:')]"))
-        )
-        print("Text 'Текущий билет:' appeared.")
+        if entered_value == f"{number_card}":
+            print("наконец-то")
+            button = WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, ".g-btn.m-btn-lg.request-btn"))
+            )
+            button.click()
+            print("Button clicked")
 
-        ticket_text = ticket_element.text
-        print(f"Found text: {ticket_text}")
+            print("Waiting for text 'Текущий билет:' to appear...")
+            ticket_element = WebDriverWait(driver, 15).until(
+                EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Текущий билет:')]"))
+            )
+            print("Text 'Текущий билет:' appeared.")
 
-except Exception as e:
-    print(f"Element not found or another error occurred: {e}")
-finally:
-    driver.quit()
+            ticket_text = ticket_element.text
+            print(f"Found text: {ticket_text}")
+
+    except Exception as e:
+        print(f"Element not found or another error occurred: {e}")
+    finally:
+        driver.quit()
+
+get_data_money("000464198")
